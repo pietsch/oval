c4c5c16197e590a0a30433050d90e44598606a03
Mathias Loesch <m.loesch83@gmail.com>
2011-02-04 14:12:49 +0100

    Wrote setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from setuptools import setup, find_packages
+from distutils import log
+
+import ovid
+
+long_desc = '''
+ovid is a validator for OAI-PMH interfaces. It checks XML validity as 
+well as conformance of OAI-PMH interface to the protocol specification.
+'''
+
+setup(
+    name='ovid',
+    version=ovid.__version__,
+    url='http://www.ub.uni-bielefeld.de/',
+    license='BSD',
+    author='Mathias Loesch',
+    author_email='Mathias.Loesch@uni-bielefeld.de',
+    description='Validator for OAI-PMH interfaces',
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Intended Audience :: Science/Research',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+)
