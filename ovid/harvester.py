a0919b936fd3331925dc6c4c22491da7a69bb345
Mathias Loesch <m.loesch83@gmail.com>
2011-02-10 10:40:29 +0100

    Added check for incremental harvesting.

--- ovid/harvester.py
+++ ovid/harvester.py
@@ -12,8 +12,15 @@ import urllib2
 from urllib import urlencode
 
 def request_oai(base_url, verb, **kw):
-    """Perform request to base_url with verb and kw args. Return file like."""
+    """
+        Perform request to base_url with verb and OAI args. Return file like.
+        Note that "from" is a reserved word in Python; use "_from" instead.
+    """
     params = kw
     params['verb'] = verb
+    # from is a reserved word in Python; use _from instead
+    if "_from" in params.keys():
+        params['from'] = params['_from']
+        del params['_from']
     url = base_url + urlencode(params)
     return urllib2.urlopen(url)
--- ovid/validator.py
+++ ovid/validator.py
@@ -11,14 +11,23 @@
 import sys
 import os
 
+import random
 import urllib2
-from urllib2 import URLError
+from urllib2 import URLError, HTTPError
+
 from lxml import etree
 from lxml.etree import XMLSyntaxError
 from lxml.etree import DocumentInvalid
 
 from harvester import request_oai
 from ovid import DATA_PATH
+
+OAI_NAMESPACE = "http://www.openarchives.org/OAI/2.0/"
+OAI = '{%s}' % OAI_NAMESPACE
+
+DC_NAMESPACE = "http://purl.org/dc/elements/1.1/"
+DC = '{%s}' % DC_NAMESPACE
+
 
 class Validator(object):
     """Validate OAI-OMH interfaces"""
@@ -38,7 +47,7 @@ class Validator(object):
             return str(m)
         except ValueError, m:
             return str(m)
-        except urllib2.HTTPError, e:
+        except HTTPError, e:
             return e.code
     
     
@@ -84,5 +93,49 @@ class Validator(object):
         except DocumentInvalid, message:
             return message
 
+    def reasonable_batch_size(self, verb, min_batch_size=50, max_batch_size=200):
+        """
+        Check if a reasonable number of data records is returned for a
+        ListRecords/ListIdentifiers request. Return a tuple of
+        (result, actual batch size). Result can be -1 (to small), 1 (too
+        large) or 0 (reasonable size).
+        """
+        if verb == 'ListRecords':
+            element = 'record'
+        if verb == 'ListIdentifiers':
+            element = 'header'
+        remote = request_oai(self.base_url, verb, metadataPrefix='oai_dc')
+        tree = etree.parse(remote)
+        records = tree.findall('.//' + OAI + element)
+        batch_size = len(records)
+        if batch_size < min_batch_size:
+            return (-1, batch_size, min_batch_size)
+        elif batch_size > max_batch_size:
+            return (1, batch_size, max_batch_size)
+        else:
+            return (0, batch_size)
+    
+    def incremental_harvesting(self):
+        """Check if server supports incremental harvesting by date."""
+        remote = request_oai(self.base_url, 'ListRecords', metadataPrefix='oai_dc')
+        tree = etree.parse(remote)
+        records = tree.findall('.//' + OAI + 'record')
+        reference_record = random.sample(records, 1)[0]
+        reference_datestamp = reference_record.find('.//' + OAI + 'datestamp').text
+        reference_oai_id = reference_record.find('.//' + OAI + 'identifier').text
+        
+        remote = request_oai(self.base_url, 'ListRecords', 
+                            metadataPrefix='oai_dc', _from=reference_datestamp, 
+                            until=reference_datestamp)
+        tree = etree.parse(remote)                    
+        records = tree.findall('.//' + OAI + 'record')
+        if len(records) > 1:
+            return False
+        test_record = records[0]
+        test_oai_id = test_record.find('.//' + OAI + 'identifier').text
+        if test_oai_id == reference_oai_id:
+            return True
+        else:
+            return False
 
         
